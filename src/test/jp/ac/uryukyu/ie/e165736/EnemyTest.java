ed269c577251875a09c8bd330add0fb6fc0b1788
Fu minghao <e165736@ie.u-ryukyu.ac.jp>
2017-01-16 22:40:25 +0900

    src/main/jp.ac.uryukyu.ie.tnal/Enemy.java src/main/jp.ac.uryukyu.ie.tnal/Hero.java src/main/jp.ac.uryukyu.ie.tnal/Main.java

---
+++ src/test/jp/ac/uryukyu/ie/e165736/EnemyTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e165736;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by e165736 on 2017/1/16.
+ */
+public class EnemyTest {
+    @org.junit.Test
+    public void attack() throws Exception { int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.hitPoint);
+    }
+
+}
