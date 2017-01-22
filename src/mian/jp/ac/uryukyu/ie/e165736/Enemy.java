025386b20a491707b7c5f22361a60abf874dcd26
Fu minghao <e165736@ie.u-ryukyu.ac.jp>
2017-01-22 18:22:56 +0900

    src/main/jp.ac.uryukyu.ie.tnal/Enemy.java src/main/jp.ac.uryukyu.ie.tnal/Hero.java src/main/jp.ac.uryukyu.ie.tnal/Main.java

--- src/mian/jp/ac/uryukyu/ie/e165736/Enemy.java
+++ src/mian/jp/ac/uryukyu/ie/e165736/Enemy.java
@@ -49,6 +49,7 @@ public class Enemy {
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
+
     }
 
     /**
